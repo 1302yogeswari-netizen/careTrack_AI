cff9677b0c85e030de996129fe33810488eacf04
1302yogeswari-netizen <1302yogeswari@gmail.com>
2026-07-19 00:34:59 +0530

    updated js file

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 
+const StorageController = {
+    STORAGE_KEY: 'caretrack_data',
+
+    getAllResidents: function(){
+        const rawData = localStorage.getItem(this.STORAGE_KEY);
+
+        return JSON.parse(rawData) || [];
+    },
+
+    saveAllResidents: function(residentsArray){
+        if(!Array.isArray(residentsArray)){
+            console.error("Storage Error: Provided data must be a valid array.");
+            return;
+        }
+
+        const stringData = JSON.stringify(residentsArray);
+        localStorage.setItem(this.STORAGE_KEY,stringData);
+    },
+
+    getResidentById: function(id){
+        const residents = this.getAllResidents();
+        const targetResident = residents.find(res => res.id === id);
+        return targetResident || null;
+    }
+};
